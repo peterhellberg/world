259b2b19c8d3b522bd2c39c4524a806ab519b83f
Peter Hellberg <peter@c7.se>
2014-08-24 19:47:18 +0200

    Initial countries test

---
+++ countries_test.go
@@ -0,0 +1,13 @@
+package world
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCountries(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(SE.Name, "Sweden")
+}
