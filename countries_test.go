3954b015274349c70e8b5a1ca996325b1f92dd89
Peter Hellberg <peter@c7.se>
2014-08-24 23:51:04 +0200

    More properties, Territories and Supranationals

--- countries_test.go
+++ countries_test.go
@@ -11,4 +11,25 @@ func TestCountries(t *testing.T) {
 
 	assert.Equal(SE.Name, "Sweden")
 	assert.Equal(ZA.Slug, "south-africa")
+	assert.Equal(AM.Key, "am")
+	assert.Equal(AZ.Code, "AZE")
+	assert.Equal(BG.Motor, "BG")
+	assert.Equal(BO.Alpha3, "BOL")
+	assert.Equal(CA.FIFA, "CAN")
+	assert.Equal(CU.Net, "cu")
+	assert.Equal(DJ.Region, "Africa")
+	assert.Equal(EE.Pop, 1340194)
+	assert.Equal(FI.Area, 338145)
+}
+
+func TestTerritories(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(AX.Name, "Åland Islands")
+}
+
+func TestSupranationals(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(EU.Name, "European Union")
 }
